71b8278f8bfa80d127fe0bcb0994203d3940cd32
pejman hkh <pejman-hkh@users.noreply.github.com>
2017-06-29 12:02:11 +0430

    Add files via upload

---
+++ tests/oop.php
@@ -0,0 +1,20 @@
+<?4php
+
+$a = new test();
+
+print_r( $a );
+
+$a->pejmanhkh( "test", 1, 2 );
+
+echo("dadsas\n");
+
+$a->ppp( "test" );
+
+
+echo("dadsas\n");
+
+$aa = new test();
+$aa->pejmanhkh( "test", 1, 2 );
+
+
+?>
